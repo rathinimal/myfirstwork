f9189d862df155d74c4997535ee9dda3b7d51aaf
rathinimal <rathinarasnimal@gmail.com>
2024-06-19 00:17:51 +0530

    The scalafile upoloade

--- Helloworld.scala
+++ Helloworld.scala
@@ -1,5 +1,9 @@
 object HelloWorld {
   def main(args: Array[String]): Unit = {
-    println("Hello, world!")
+    def maximum(a:Int,b:Int): Int = a>=b match{
+      case true => a
+      case false => b
+      }
+    print(maximum(3,12))
   }
 }
